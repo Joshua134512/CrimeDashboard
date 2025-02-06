5e80f872bfc289314a36bbf8ed9efd6bb686b474
Joshua Morningstar <joshmst@iastate.edu>
2025-02-06 15:35:16 -0600

    exception if no command given

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from execution.execute import execute_functions, list_commands
 import sys
 
 def main():
-    input = sys.argv[1]
+    try:
+        input = sys.argv[1]
+    except:
+        raise Exception('main.py requires a command to run. Run "python main.py help" to get started')
     if input == 'help':
         print('Commands can be found in --- execution > commands.yml --- \n alternatively, type "python main.py list" for a list of commands \n execute a command with "python main.py <command_name>" in the terminal')
     elif input == 'list':
