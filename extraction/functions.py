e56ac77f93fc0d7f64fa218ee2e58fbd4821dec9
Joshua Morningstar <joshmst@iastate.edu>
2025-02-10 11:40:57 -0600

    automatically download data for all 50 states

--- extraction/functions.py
+++ extraction/functions.py
@@ -4,6 +4,8 @@ from os import path,curdir,makedirs
 
 ROOT = path.abspath(curdir)
 
+STATES = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
 def download_file(url: str, filepath: str, **_):
     with Extractor(url) as extract:
         contents = extract.download_csv()
@@ -11,25 +13,13 @@ def download_file(url: str, filepath: str, **_):
         writer.write(contents)
     return
 
-def download_files(**kwargs):
-    download_option = kwargs.get('download_option') or 'all'
-    source = kwargs.get('source') or 'data'
-    files = settings[source]
-    for key in files:
-        file = key + '.csv'
-        folderpath = path.join(ROOT, source)
-        if not path.exists(folderpath):
-            makedirs(folderpath)
-        filepath = path.join(folderpath, file)
-        if download_option == 'all':
-            print(f"Downloading {key} data file")
-            url = settings.data[key]["url"]
-            download_file(url, filepath)
-        elif download_option == 'new':
-            if path.exists(filepath):
-                print(f"{key} data file already downloaded: Skipping")
-            else:
-                print(f"Downloading {key} data file")
-                url = settings.data[key]["url"]
-                download_file(url, filepath)
-    print("Downloads complete")
+def download_files(url, folder, **_):
+    api_key = settings.fbi.key
+    folderpath = path.join(ROOT, "data", folder)
+    if not path.exists(folderpath):
+        makedirs(folderpath)
+    for state in STATES:
+        print(f"Downloading {state} {folder} data")
+        url = url.format(state = state, api_key = api_key)
+        file = f"{state}.csv"
+        download_file(url, path.join(folderpath, file))
