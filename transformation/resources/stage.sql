67cc226f6c6425cc706e36f6826905cfce6656f8
Joshua Morningstar <joshmst@iastate.edu>
2025-02-19 09:54:01 -0600

    only one state column

--- transformation/resources/stage.sql
+++ transformation/resources/stage.sql
@@ -1,4 +1,15 @@
-create table staging as select * from
+create table staging as select p.state,
+    r.recidivismrate,
+    r.prisonpopulation,
+    r.imprisonmentrate,
+    p.population,
+    q.qualityoflifetotalscore,
+    q.qualityoflifequalityoflife,
+    q.qualityoflifeaffordability,
+    q.qualityoflifeeconomy,
+    q.qualityoflifeeducationandhealth,
+    q.qualityoflifesafety
+ from
     population as p
         inner join recidivism as r on p.state = r.state
         inner join qol as q on q.state = p.state;
