350ee060f5cfb5b1531e6851d020f5e8593a8430
Joshua Morningstar <joshmst@iastate.edu>
2025-02-24 09:55:20 -0600

    utility table

--- transformation/resources/stage.sql
+++ transformation/resources/stage.sql
@@ -12,4 +12,6 @@ create table stage as select p.state,
  from
     population as p
         outer left join recidivism as r on p.state = r.state
-        outer left join qol as q on p.state = q.state;
+        outer left join qol as q on p.state = q.state
+        outer left join utility as u on p.state = u.state
+        outer left join arrests as a on u.abbreviation = a.state;
