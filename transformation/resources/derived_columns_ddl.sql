275a9d9bb125f7bcfac6c18bf075f94cd0cc1097
Joshua Morningstar <joshmst@iastate.edu>
2025-03-14 10:14:39 -0500

    calculated fields in new file

---
+++ transformation/resources/derived_columns_ddl.sql
@@ -0,0 +1,16 @@
+drop table if exists derived_columns_stage;
+create table derived_columns_stage(
+    State varchar(255),
+    RecidivismRate varchar(255),
+    PercentMale varchar(255),
+    PercentFemale varchar(255),
+    PercentAsian varchar(255),
+    PercentWhite varchar(255),
+    PercentBlack varchar(255),
+    PercentNativeAmerican varchar(255),
+    PercentOfPopInPrison varchar(255),
+    TotalViolentCrimes varchar(255),
+    TotalNonviolentCrimes varchar(255),
+    OtherCrimes varchar(255),
+    AverageQOLScore varchar(255)
+)
